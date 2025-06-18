5ff2bb115b4d01645fc36e9335a26c1d3ea43a21
DelffyngsGO <fcristiansilverazhang@gmail.com>
2025-06-18 15:46:54 -0300

    Add files via upload

---
+++ juca5.5a-script.js
@@ -0,0 +1,11 @@
+// 5.5a
+
+let nota1 = Number(prompt("Insira sua primeira nota: "))
+let nota2 = Number(prompt("Insira sua segunda nota: "))
+let resultado = (nota1 + nota2) / 2
+
+if(resultado >= 7){
+    alert("Você passou! ")
+} else {
+    alert("Você reprovou :( ")
+}
---
+++ juca5.6-script.js
@@ -0,0 +1,10 @@
+let pontuação = Number(prompt("Insira sua pontuação: "))
+if (pontuação <= 10) {
+    alert("Deu ruim ")
+} else if (pontuação > 10 , or <= 100) {
+    alert("Tá... melhorando hein ")
+} else if (pontuação > 100 , or <= 200) {
+    alert("SUPIMPA! ")
+} else if (pontuação > 200) {
+    alert("MITOU!!")
+}
